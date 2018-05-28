3c67d04855f6ea71a8493a094551f474183fb7c8
hfourie <32088944+hfourie@users.noreply.github.com>
2018-05-28 21:33:26 +0200

    Page Updates
    Added Tabs
    Color, Size and Nicotine
    Added in all the fields from the product Maint page

---
+++ src/Vape.CMS.DAL/DTO/ProductDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vape.CMS.DAL.Entities;
+
+namespace Vape.CMS.DAL.DTO
+{
+    public class ProductDto
+    {
+        public Product Product { get; set; }
+        public ProductColorTie ProductColorTie { get; set; }
+        public ProductNicotineTie ProductNicotineTie { get; set; }
+        public ProductSizeTie ProductSizeTie { get; set; }
+    }
+}
